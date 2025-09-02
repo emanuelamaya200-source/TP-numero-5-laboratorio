0d274313521510fbde937649f74df8cd9973dc00
Lucas <serranolucas1000@gmail.com>
2025-09-02 20:12:11 -0300

    Diseño internal frame
    internal frame buscar cliente por ciudad creado

--- TrabajoN5/src/trabajon5/Jframe/Directorio/BuscarClientePorCiudad.java
+++ TrabajoN5/src/trabajon5/Jframe/Directorio/BuscarClientePorCiudad.java
@@ -26,21 +26,91 @@ public class BuscarClientePorCiudad extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        Tabla_BCDDPC = new javax.swing.JTable();
+        BCDDPC = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        Cb_Ciudades = new javax.swing.JComboBox<>();
+        jButton1 = new javax.swing.JButton();
+
+        Tabla_BCDDPC.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "DNI", "Apellido", "Nombre", "Direccion", "Ciudad", "Telefono"
+            }
+        ));
+        jScrollPane1.setViewportView(Tabla_BCDDPC);
+
+        BCDDPC.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        BCDDPC.setText("Buscar Clientes de Directorio por Ciudad");
+
+        jLabel1.setText("Ciudades:");
+
+        jButton1.setText("Salir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addGap(0, 109, Short.MAX_VALUE))
+                            .addComponent(Cb_Ciudades, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(BCDDPC)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 485, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton1)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(9, 9, 9)
+                .addComponent(BCDDPC)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(Cb_Ciudades, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1)
+                .addContainerGap(29, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton1ActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BCDDPC;
+    private javax.swing.JComboBox<String> Cb_Ciudades;
+    private javax.swing.JTable Tabla_BCDDPC;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
--- TrabajoN5/src/trabajon5/Jframe/Directorio/BuscarTelPorApellido.java
+++ TrabajoN5/src/trabajon5/Jframe/Directorio/BuscarTelPorApellido.java
@@ -29,7 +29,7 @@ public class BuscarTelPorApellido extends javax.swing.JInternalFrame {
         JLabel_Apellido = new javax.swing.JLabel();
         BuscarApellido = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        Tabla_BTPA = new javax.swing.JTable();
         jScrollPane2 = new javax.swing.JScrollPane();
         LisraApellidos = new javax.swing.JList<>();
         jLabel1 = new javax.swing.JLabel();
@@ -43,7 +43,7 @@ public class BuscarTelPorApellido extends javax.swing.JInternalFrame {
             }
         });
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        Tabla_BTPA.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null, null, null},
                 {null, null, null, null, null, null},
@@ -62,7 +62,7 @@ public class BuscarTelPorApellido extends javax.swing.JInternalFrame {
                 return types [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(Tabla_BTPA);
 
         jScrollPane2.setViewportView(LisraApellidos);
 
@@ -135,9 +135,9 @@ public class BuscarTelPorApellido extends javax.swing.JInternalFrame {
     private javax.swing.JTextField BuscarApellido;
     private javax.swing.JLabel JLabel_Apellido;
     private javax.swing.JList<String> LisraApellidos;
+    private javax.swing.JTable Tabla_BTPA;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
