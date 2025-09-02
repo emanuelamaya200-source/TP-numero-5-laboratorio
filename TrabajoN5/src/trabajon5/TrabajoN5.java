7d78256b0ab44037770c3288cff782e885281c6d
emanuelamaya200-source <emanuelamaya200@gmail.com>
2025-09-02 19:07:04 -0300

    Base tp 4
    Base tp 4

---
+++ TrabajoN5/src/trabajon5/TrabajoN5.java
@@ -0,0 +1,21 @@
+
+package trabajon5;
+
+/**
+ * Amaya Leandro
+ * Sosa Daiana
+ * D'AGata Fabian
+ * Serrano lucas
+ * Teo nicolas Miranda
+ * 
+ */
+public class TrabajoN5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
